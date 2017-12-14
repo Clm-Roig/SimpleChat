425183e70a199ccd77acb5f65ed58b6108c41fe4
Clément Roig <clm.roig@gmail.com>
2017-12-14 14:12:11 +0100

    v0 fonctionne !

--- simplechat4/ClientFenetre.java
+++ simplechat4/ClientFenetre.java
@@ -1,29 +1,31 @@
 import java.awt.Dimension;
+import java.awt.event.*;
 import java.io.*;
 import client.*;
 import common.*;
 import javax.swing.*;
 import java.awt.Color;
 
-public class ClientFenetre extends JFrame implements ChatIF {
+public class ClientFenetre extends JFrame implements ChatIF, ActionListener, KeyListener {
 
 	//Class variables *************************************************
 	final public static int DEFAULT_PORT = 5555;
 
 	//Instance variables **********************************************
-	
+
 	final private static int WINDOW_WIDTH = 500;
 	final private static int WINDOW_HEIGHT = 500;
 	final private static int MSG_FONT_SIZE = 20;
 	final private static int MARGIN = 10;
 
+	final private static String APP_NAME = "Simple Chat 4";
 
-	ChatClient client;
-	private JPanel mainPanel;
-	private JPanel displayPanel;
-	private JPanel interactPanel;
-	private JTextField textFChat;
-	private JButton buttonSend;
+	ChatClient 			client;
+	private JPanel 		mainPanel;
+	private JTextArea 	displayArea;
+	private JPanel 		interactPanel;
+	private JTextField 	textFChat;
+	private JButton 	buttonSend;
 
 	//Constructors ****************************************************
 
@@ -34,10 +36,10 @@ public class ClientFenetre extends JFrame implements ChatIF {
 	public ClientFenetre(String host, int port) {
 		super();
 		try {
-			client= new ChatClient(host, port, this);
+			client = new ChatClient(host, port, this);
 		} 
 		catch(IOException exception) {
-			System.out.println("Error: Can't setup connection!"
+			System.out.println("Error: Can't create the Client !"
 					+ " Terminating client.");
 			System.exit(1);
 		}
@@ -50,6 +52,42 @@ public class ClientFenetre extends JFrame implements ChatIF {
 	 */
 	public ClientFenetre(String host, int port, String idClient) {
 		super();
+
+		// Interaction Panel
+		this.textFChat = new JTextField();
+		textFChat.setPreferredSize(new Dimension(WINDOW_WIDTH - 80 - MARGIN, MSG_FONT_SIZE));
+		textFChat.addKeyListener(this);
+
+		this.buttonSend = new JButton("Send");
+		buttonSend.setPreferredSize(new Dimension(80 - MARGIN, MSG_FONT_SIZE));
+		buttonSend.addActionListener(this);
+
+		this.interactPanel = new JPanel();
+		interactPanel.add(textFChat);
+		interactPanel.add(buttonSend);
+
+		// Displaying messages Area
+		this.displayArea = new JTextArea();
+		displayArea.setLineWrap(true);
+		displayArea.setEditable(false);
+		displayArea.setPreferredSize(new Dimension(WINDOW_WIDTH - MARGIN, WINDOW_HEIGHT - MSG_FONT_SIZE - 2*MARGIN));
+		displayArea.setBackground(Color.lightGray);
+
+		// MainPanel Configuration
+		this.mainPanel = new JPanel();	 
+		mainPanel.add(displayArea);
+		mainPanel.add(interactPanel);	    
+
+		// Window Configuration	   
+		this.setTitle(APP_NAME);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+		this.setContentPane(mainPanel); 	    
+		this.setLocationRelativeTo(null);
+		
+		display("Welcome in "+APP_NAME);
+
 		try {
 			client = new ChatClient(host, port, idClient, this);
 		} 
@@ -60,40 +98,44 @@ public class ClientFenetre extends JFrame implements ChatIF {
 
 			System.exit(1);
 		}
-			    
-	    // Interaction Panel
-	    this.textFChat = new JTextField();
-	    textFChat.setPreferredSize(new Dimension(WINDOW_WIDTH - 80 - MARGIN, MSG_FONT_SIZE));
-	    this.buttonSend = new JButton("Send");
-	    buttonSend.setPreferredSize(new Dimension(80 - MARGIN, MSG_FONT_SIZE));
-	    
-	    this.interactPanel = new JPanel();
-	    interactPanel.add(textFChat);
-	    interactPanel.add(buttonSend);
-	    
-	    // Display Panel
-	    this.displayPanel = new JPanel();
-	    displayPanel.setPreferredSize(new Dimension(WINDOW_WIDTH - MARGIN, WINDOW_HEIGHT - MSG_FONT_SIZE - 2*MARGIN));
-	    displayPanel.setBackground(Color.GRAY);
-	    
-	    // MainPanel Configuration
-	    this.mainPanel = new JPanel();	 
-	    mainPanel.add(displayPanel);
-	    mainPanel.add(interactPanel);	    
-
-	    // Window Configuration	   
-	    this.setTitle("SimpleChat v4");
-	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	  
-	    this.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
-	    this.setContentPane(mainPanel); 	    
-	    this.setLocationRelativeTo(null);
 	}
 
 	@Override
 	public void display(String message) {
-		// TODO Auto-generated method stub
+		this.displayArea.append(message + "\n");
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		String text = textFChat.getText();
+
+		if(event.getSource() == this.buttonSend) {
+			if(text != null && !text.trim().isEmpty()) {		
+				client.handleMessageFromClientUI(text);
+			}
+		}
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_ENTER) {
+			String text = textFChat.getText();	 	
+			if(text != null && !text.trim().isEmpty()) {		
+				client.handleMessageFromClientUI(text);
+			}
+		}		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 	}
 
 
@@ -132,7 +174,7 @@ public class ClientFenetre extends JFrame implements ChatIF {
 
 		ClientFenetre chat = new ClientFenetre(host, port, idClient);
 		chat.setVisible(true);
-		System.out.println("Client running !");
 	}
+
 
 }
